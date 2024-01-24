4b412d769805a9e27c3dd049d1fd8e17d6f3b18d
Doug Gordon <gordononline@bigpond.com>
2024-01-24 17:34:39 +1100

    tkinter

---
+++ import tkinter.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Welcome")
+
+root.geometry("600x400")
+
+menu = tk.Menu(root)
+item = tk.Menu(menu)
+item.add_command(label="New")
+menu.add_cascade(label="File", menu=item)
+root.config(menu=menu)
+
+
+lbl = tk.Label(root, text="Are you a Geek?")
+lbl.grid()
+
+# adding Entry Field
+txt = tk.Entry(root, width=10)
+txt.grid(column=1, row=0)
+
+
+# function to display user text when
+# button is clicked
+def clicked():
+    res = f"You wrote {txt.get()}"
+    lbl.configure(text=res)
+
+
+# button widget with red color text inside
+btn = tk.Button(root, text="Click me", fg="red", command=clicked)
+# Set Button Grid
+btn.grid(column=2, row=0)
+
+root.mainloop()
